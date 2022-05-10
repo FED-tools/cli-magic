61b90a805892d2388db7d985c221b9ae4d2d6408
Artem Deikun <artemhp@gmail.com>
2022-05-10 11:36:39 +0300

    #21 Core Package in Monorepo

--- packages/core/src/clone/index.js
+++ packages/core/src/clone/index.js
@@ -3,23 +3,27 @@ import fs from 'fs';
 
 const clone = async ({ list, path, log }) => {
   let countRepos = 0;
+  let clonedRepos = [];
   log(`Cloning all repositories (${list.length})`);
-  for await (let repo of list) {
-    const pathCurrent = `${path}/${repo.path}`;
-    log(`Start cloning: (${++countRepos}/${list.length}) ${repo.repo} : `);
+  for await (let el of list) {
+    const pathCurrent = `${path}/${el.path}`;
+    log(`Start cloning: (${++countRepos}/${list.length}) ${el.repo} : `);
     try {
       fs.accessSync(pathCurrent, fs.constants.F_OK);
       log(`Repository already exists: ${pathCurrent}`);
     } catch (err) {
-      const command = `git clone ${repo.repo} ${pathCurrent}`;
+      const command = `git clone ${el.repo} ${pathCurrent}`;
       log('> ' + command);
       await exec(command);
-      log(`Repository has been cloned: ${repo.repo}`);
-      log(`Repository already exists here: ${pathCurrent}`);
+      log(`Repository has been cloned: ${el.repo}`);
+      clonedRepos.push(el.repo);
     }
   }
   log(`Done cloning all repositories`);
-  return list;
+  return {
+    count: countRepos,
+    repos: clonedRepos,
+  };
 };
 
 export { clone };
