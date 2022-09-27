ec9fec0bdfa140fc6b252108d9f86d436f49287f
Artem Deikun <artemhp@gmail.com>
2022-09-26 21:43:12 -0400

    #53 Init unit tests

---
+++ packages/cli/src/logic.test.ts
@@ -0,0 +1,15 @@
+import init from './logic';
+
+test('Test Create', async () => {
+  await init('create');
+});
+
+// test('Test Update', async () => {
+//   await init('update');
+// });
+// test('Test Commits', async () => {
+//   await init('commits');
+// });
+// test('Test Wrong Command', async () => {
+//   await init();
+// });
---
+++ packages/cli/src/logic.ts
@@ -0,0 +1,39 @@
+#!/usr/bin/env node
+
+import { clone, pull, commits } from '@mgct/core';
+import * as fs from 'fs';
+import { opt } from './configs/options';
+import log from './helpers/log';
+import parseCli from './yargs/parseCli';
+
+export default async function init(aaa?: string) {
+  const { actionCommand = aaa, inputArguments, pathToConfig, pathToLog, projectsDistPath } = parseCli(opt);
+  const projectsList = JSON.parse(String(fs.readFileSync(pathToConfig)));
+
+  if (actionCommand === 'create') {
+    await clone({
+      list: projectsList,
+      path: projectsDistPath,
+      log: log(pathToLog),
+    });
+  } else if (actionCommand === 'update') {
+    await pull({
+      list: projectsList,
+      path: projectsDistPath,
+      log: log(pathToLog),
+    });
+  } else if (actionCommand === 'commits') {
+    const commitsList = await commits({
+      list: projectsList,
+      path: projectsDistPath,
+      author: inputArguments.user,
+      period: inputArguments.period,
+      log: log(pathToLog),
+    });
+    // eslint-disable-next-line no-console
+    console.log(commitsList);
+  } else {
+    // eslint-disable-next-line no-console
+    console.log('Wrong Command');
+  }
+}
